1e984a9f4c1cf7eb545fd166aee3a6e91f942572
Jesse <jessemerge2@gmail.com>
2023-01-12 12:59:11 -0800

    Formatting and lint setup

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
 import { fileURLToPath, URL } from "node:url";
-
+import eslint from "@rollup/plugin-eslint";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+	plugins: [
+		vue(),
+		{
+			...eslint(),
+			enforce: "pre",
+			apply: "build",
+		},
+	],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
 });
