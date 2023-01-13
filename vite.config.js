c17c1f414aa699ae8a3d9f06e9a00bb9c89f11fd
Jesse <jessemerge2@gmail.com>
2023-01-13 10:47:31 -0800

    Prepare for deploying

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { fileURLToPath, URL } from "node:url";
-import eslint from "@rollup/plugin-eslint";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		{
-			...eslint(),
-			enforce: "pre",
-			apply: "build",
-		},
-	],
+	plugins: [vue()],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
