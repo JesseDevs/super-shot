c059f6eb7748fa39470b78f34d3aa1ad961a1e0b
Jesse <jessemerge2@gmail.com>
2023-01-14 02:08:53 -0800

    Add rules to avoid problems

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,10 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
 	root: true,
 	extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/eslint-config-prettier"],
+	rules: {
+		"no-unused-vars": "off",
+		"vue/require-v-for-key": "off",
+	},
 	parserOptions: {
 		ecmaVersion: "latest",
 	},
