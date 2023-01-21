5a6ab9df5208c0dce4ec22c038854de055edc0d1
Jesse <jessemerge2@gmail.com>
2023-01-20 19:21:14 -0800

    Make responsive icon component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 	import { RouterView } from "vue-router";
 	import SiteHeader from "./components/SiteHeader.vue";
 	import SiteFooter from "./components/SiteFooter.vue";
+	import SpriteSheet from "./components/SpriteSheet.vue";
 </script>
 
 <template>
 	<vue-app>
+		<SpriteSheet />
 		<SiteHeader />
 
 		<main class="outlet">
