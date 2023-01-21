872ed13fc10fd79ee1c4a231c908e4291c2bc0a8
Jesse <jessemerge2@gmail.com>
2023-01-20 19:21:40 -0800

    …

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,18 @@
+<script setup>
+	defineProps(["icon"]);
+</script>
+<template>
+	<div class="contains-svg">
+		<svg :class="icon"><use :xlink:href="`#icon-${icon}`"></use></svg>
+	</div>
+</template>
+
+<style lang="scss">
+	div.contains-svg {
+		width: 40px;
+		height: 40px;
+		svg * {
+			fill: black;
+		}
+	}
+</style>
