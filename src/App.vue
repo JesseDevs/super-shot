d4b7fd49bec7f7f6c609239d2f005ce21f50a285
Jesse <jessemerge2@gmail.com>
2023-01-23 23:52:23 -0800

    Add spacing for checkout button

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup>
+	import { computed } from "vue";
 	import { RouterView } from "vue-router";
 	import SiteHeader from "./components/SiteHeader.vue";
 	import SiteFooter from "./components/SiteFooter.vue";
 	import SpriteSheet from "./components/SpriteSheet.vue";
+	import { useCartStore } from "@/stores/cart";
+	import { useInterfaceStore } from "@/stores/interface";
+
+	const ui = useInterfaceStore();
+	const cart = useCartStore();
+
+	const addSpace = computed(function () {
+		return ui.mainMenuOpen === false && cart.itemsInCart !== 0 ? "addSpace" : "";
+	});
 </script>
 
 <template>
@@ -10,7 +20,7 @@
 		<SpriteSheet />
 		<SiteHeader />
 
-		<main class="outlet">
+		<main :class="`outlet ${addSpace}`">
 			<RouterView />
 		</main>
 
@@ -18,4 +28,10 @@
 	</vue-app>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+	main.addSpace {
+		section > inner-column {
+			padding-bottom: 100px;
+		}
+	}
+</style>
