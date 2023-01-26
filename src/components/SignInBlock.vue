0b0463d5419962ef9a5f74fb4f09ac5c36d410f3
Jesse <jessemerge2@gmail.com>
2023-01-25 16:47:19 -0800

    Renaming components

---
+++ src/components/SignInBlock.vue
@@ -0,0 +1,30 @@
+<script setup>
+	import { RouterLink } from "vue-router";
+	import { useInterfaceStore } from "@/stores/interface";
+
+	import { useProfilesStore } from "../stores/profile";
+
+	const profiles = useProfilesStore();
+	const ui = useInterfaceStore();
+</script>
+
+<template>
+	<signin-block v-if="!profiles.isLoggedIn">
+		<RouterLink class="small-voice" to="/sign-in" @click="ui.toggleMenu()">Sign In</RouterLink>
+		<RouterLink class="small-voice" @click="ui.toggleMenu()" to="/sign-up"> Sign Up </RouterLink>
+		<span class="tiny-voice">Not a Dunkin' Rewards memeber?</span>
+	</signin-block>
+
+	<signin-block v-else>
+		<h3 class="strict-voice">Hi, {{ profiles.currentUser.name }}</h3>
+		<RouterLink
+			class="small-voice"
+			to="/create"
+			@click="ui.toggleMenu()"
+			:v-if="profiles.currentUser.role == 'admin'"
+			>Create</RouterLink
+		>
+		<RouterLink class="small-voice" @click="ui.toggleMenu()" to="/sign-up"> Sign Out </RouterLink>
+		<span class="tiny-voice">Checkout your Dunkin' Rewards</span>
+	</signin-block>
+</template>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,24 @@
+<script setup>
+	import CheckoutBtn from "../components/CheckoutBtn.vue";
+	import ModuleThing from "../components/ModuleThing.vue";
+	import { useProfilesStore } from "../stores/profile";
+
+	const profiles = useProfilesStore();
+</script>
+
+<template>
+	<section>
+		<inner-column>
+			<home-module>
+				<ModuleThing />
+
+				<RouterLink class="button" to="/sign-in">Sign In</RouterLink>
+				<RouterView />
+
+				<CheckoutBtn />
+			</home-module>
+		</inner-column>
+	</section>
+</template>
+
+<style lang="scss"></style>
