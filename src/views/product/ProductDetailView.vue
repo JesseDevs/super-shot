0a972c91a920949b60ae10a80887029cd588932e
Jesse <jessemerge2@gmail.com>
2023-01-28 20:28:20 -0800

    Create edit form

--- src/views/product/ProductDetailView.vue
+++ src/views/product/ProductDetailView.vue
@@ -1,26 +1,49 @@
 <script setup>
-	import { computed } from "vue";
+	import { computed, reactive } from "vue";
 	import { useRoute } from "vue-router";
 	import { useMenuStore } from "@/stores/menu";
 	import { useCartStore } from "@/stores/cart";
+	import SvgIcon from "@/components/SvgIcon.vue";
+	import slugid from "slugid";
 
 	const cart = useCartStore();
 	const route = useRoute();
 	const menu = useMenuStore();
+	const sizes = ["s", "m", "l", "xl"];
+	const dairies = ["None", "Coconutmilk", "Oatmilk", " Whole Milk", "Skim Milk"];
+	const sweets = ["None", "Sugar"];
+	const flavors = ["None", "Caramel Swirl", "French Vanilla", "Mocha Swirl"];
 
-	const category = menu.categories.find(function (record) {
-		return record.slug == route.params.slug;
+	const product = computed(function () {
+		return menu.products.find(function (record) {
+			return record.id == route.params.id;
+		});
 	});
 
-	const products = category.products;
-
-	const product = products.find(function (record) {
-		return record.id == route.params.id;
+	const newAdditions = reactive({
+		size: "m",
+		dairy: "None",
+		flavor: "None",
+		sweet: "None",
 	});
 
-	function selectSize(item) {
-		product.sizes.forEach((i) => (i.selected = false));
-		item.selected = true;
+	const newID = computed(function () {
+		return slugid.nice();
+	});
+
+	function saveNewProduct() {
+		const newRecord = { ...product };
+		newRecord.id = newID;
+
+		// const record = {
+		// 	id: slugid.nice(),
+		// 	name: product.value.name,
+		// 	slug: product.value.name.toLowerCase().replace(/ /g, "-"),
+		// 	desc: product.value.desc,
+		// 	category: product.value.category.toLowerCase(),
+		// 	image: product.value.imageURL,
+		// };
+		// cart.addItem(record);
 	}
 </script>
 
@@ -31,46 +54,183 @@
 				<img :src="`${product.imageURL}`" alt="iced" loading="lazy" />
 			</picture>
 			<h2 class="chant-voice">{{ product.name }}</h2>
-			<p>{{ product.tagline }}</p>
+			<p class="tag">{{ product.tagline }}</p>
 			<p class="intro">
 				{{ product.desc }}
 			</p>
 
-			<button class="button" @click="cart.addItem(product)">Add to cart</button>
+			<button class="button" @click="saveNewProduct()">Add to cart</button>
 		</landing-block>
-		<p>Size</p>
-		<div class="cup-size">
-			<div v-for="size in product.sizes" @click="selectSize(size)" :class="{ selected: size.selected }">
-				{{ size.size }}
+
+		<form class="main-form edit-form" autocomplete="off" @submit.prevent="save()">
+			<div class="size-block" v-if="route.params.slug === 'coffee'">
+				{{ newAdditions }}
+				<h4 class="calm-voice">Size</h4>
+
+				<ul class="cup-size">
+					<li v-for="size in sizes">
+						<label :for="size">
+							<input
+								class="radio-button"
+								type="radio"
+								:id="size"
+								name="size"
+								:value="size"
+								:aria-label="size"
+								:key="size"
+								v-model="newAdditions.size"
+							/>
+							<span> {{ size.toUpperCase() }}</span>
+						</label>
+					</li>
+				</ul>
 			</div>
-		</div>
+
+			<div class="dairy-block" v-if="route.params.slug === 'coffee'">
+				<h4 class="calm-voice">Dairy</h4>
+
+				<div class="select-box-container">
+					<select v-model="newAdditions.dairy" class="custom-select">
+						<option v-for="dairy in dairies" :value="dairy" :key="dairy">{{ dairy }}</option>
+					</select>
+					<span> <SvgIcon icon="angle-down" /></span>
+				</div>
+			</div>
+
+			<div class="sweetener-block" v-if="route.params.slug === 'coffee'">
+				<h4 class="calm-voice">Sweetener</h4>
+
+				<div class="select-box-container">
+					<select v-model="newAdditions.sweet" class="custom-select">
+						<option v-for="sweet in sweets" :value="sweet" :key="sweet">{{ sweet }}</option>
+					</select>
+					<span> <SvgIcon icon="angle-down" /></span>
+				</div>
+			</div>
+
+			<div class="flavor-block" v-if="route.params.slug === 'coffee'">
+				<h4 class="calm-voice">Flavor</h4>
+
+				<div class="select-box-container">
+					<select v-model="newAdditions.flavor" class="custom-select">
+						<option v-for="flavor in flavors" :value="flavor" :key="flavor">{{ flavor }}</option>
+					</select>
+					<span> <SvgIcon icon="angle-down" /></span>
+				</div>
+			</div>
+		</form>
 	</edit-block>
 </template>
 
 <style lang="scss">
-	div.cup-size {
+	.edit-form {
+		min-width: 280px;
+	}
+	edit-block {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 8px;
+
+		landing-block {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			text-align: center;
+			gap: 10px;
+			picture.item-picture img {
+				max-height: 160px;
+			}
+			h2 {
+				font-weight: 700;
+			}
+			p.tag {
+				font-weight: 500;
+				margin: 5px 0;
+			}
+			.intro {
+				font-size: clamp(0.65rem, calc(0.63rem + 0.13vw), 0.72rem);
+				max-width: 53ch;
+			}
+
+			@media (min-width: 600px) {
+				.intro {
+					font-size: clamp(0.94rem, calc(0.87rem + 0.34vw), 1.13rem);
+					max-width: 64ch;
+				}
+			}
+		}
+	}
+
+	.custom-select {
+		appearance: none;
+	}
+
+	div.select-box-container {
+		position: relative;
+
+		select {
+			border-radius: 50px;
+			width: 100%;
+			font-family: "Sofia Sans", sans-serif;
+			padding: 10px 20px;
+			font-size: 1rem;
+			border: 1.5px solid var(--support-soft);
+			outline: none;
+			resize: none;
+			font-weight: 500;
+		}
+
+		span {
+			position: absolute;
+			top: 7px;
+			right: 3px;
+			pointer-events: none;
+		}
+	}
+
+	ul.cup-size {
 		display: flex;
 		gap: 10px;
 
-		div {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			border-radius: 50px;
-			border: 2px solid black;
-			font-weight: 500;
-			background-color: var(--page-soft);
-
-			transition: background-color ease-in-out 300ms;
+		label {
+			cursor: default;
+			display: block;
 			width: 40px;
 			height: 40px;
-			text-transform: uppercase;
-			cursor: pointer;
+			position: relative;
+		}
+		.radio-button {
+			-webkit-appearance: none;
+			-moz-appearance: none;
+			appearance: none;
+			width: 100%;
+			height: 100%;
+			border-radius: 50%;
+			border: 1.5px solid var(--support-soft);
+			position: absolute;
+			outline: 0;
 		}
 
-		div.selected {
-			color: var(--page);
+		.radio-button + span {
+			display: block;
+			display: grid;
+			place-items: center;
+			width: 40px;
+			height: 40px;
+			text-align: center;
+			font-weight: 700;
+		}
+
+		.radio-button:checked {
+			outline: 0;
 			background-color: var(--off-color);
+		}
+
+		.radio-button:checked + span {
+			color: var(--page);
 		}
 	}
 </style>
