43808aeb55d26c6f63a51dc59ceef685134fecf7
Jesse <jessemerge2@gmail.com>
2023-01-28 20:25:34 -0800

    Stying out cart modal

--- src/components/CartModal.vue
+++ src/components/CartModal.vue
@@ -9,36 +9,44 @@
 
 <template>
 	<cart-modal>
-		<h2 class="chant-voice">Cart</h2>
-		{{ cart.checkoutTotal }}
+		<inner-column>
+			<h2 class="chant-voice">Cart</h2>
+			{{ cart.checkoutTotal }}
 
-		<ul>
-			<li v-for="item in cart.purchasingCart">{{ item.name }} - {{ item.price }}</li>
-		</ul>
+			<ul>
+				<li v-for="item in cart.purchasingCart">{{ item.name }} - {{ item.price }}</li>
+			</ul>
+		</inner-column>
 	</cart-modal>
 </template>
 
 <style lang="scss">
 	cart-modal {
-		position: fixed;
-		background-color: var(--off-color);
-		top: 75px;
+		position: absolute;
+		background-color: var(--off-color-soft);
 		right: 0;
+		top: 0;
 		width: 100%;
 		height: 100%;
+		max-height: 85vh;
 
-		display: block;
-		place-items: center;
+		border-top-left-radius: 25px;
+		border-bottom-left-radius: 25px;
+
+		display: flex;
+		align-items: flex-start;
+		flex-direction: column;
 
 		overflow-y: auto;
-		opacity: 0;
+		opacity: 0.4;
+		transform: translate(98%, 0);
 		z-index: 200;
 		pointer-events: none;
 		transition: all 0.3s ease-in-out;
 	}
-	header.cart-open cart-modal {
+	.cart-open cart-modal {
 		opacity: 1;
-		transform: translate(0, 0);
+		transform: translate(35%, 0);
 		pointer-events: initial;
 	}
 
