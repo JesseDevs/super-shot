e753fc4efbe073120367f6ad40aa26f0dd885760
Jesse <jessemerge2@gmail.com>
2023-01-31 22:22:10 -0800

    Develop cart modal adding new functions and showin

--- src/components/CartModal.vue
+++ src/components/CartModal.vue
@@ -3,10 +3,24 @@
 	import { useInterfaceStore } from "@/stores/interface";
 	import SvgIcon from "../components/SvgIcon.vue";
 	import { useProfilesStore } from "../stores/Profiles";
+	import { useRoute } from "vue-router";
 
 	const profiles = useProfilesStore();
 	const cart = useCartStore();
 	const ui = useInterfaceStore();
+	const route = useRoute();
+
+	function remove(id) {
+		cart.purchasingCart.forEach(function (item, index) {
+			if (item.id === id) {
+				cart.purchasingCart.splice(index, 1);
+			}
+		});
+	}
+
+	function increment(x) {
+		x++;
+	}
 
 	function clear() {
 		cart.purchasingCart = [];
@@ -17,17 +31,38 @@
 <template>
 	<cart-modal>
 		<h2 class="chant-voice">Shopping Cart</h2>
-		<button v-if="cart.itemsInCart" @click="clear()">
-			<SvgIcon icon="trash" />
+
+		<button v-if="!ui.editProductMode" @click="ui.toggleEditMode()" class="tiny-button tiny-voice">
+			Edit
 		</button>
 
+		<button v-else @click="ui.toggleEditMode()" class="tiny-button tiny-voice">Done</button>
+
 		<ul>
-			<li v-for="item in cart.purchasingCart">
-				{{ item.quantity }} - -{{ item.name }} - {{ item.price }}
+			<li class="item-in-cart" v-for="item in cart.purchasingCart">
+				<RouterLink :to="`/menu/${item.category}/${item.id}`">
+					<cart-card :class="`${ui.editModeClass}`">
+						<text-content>
+							<p>{{ item.quantity }}</p>
+							<p>{{ item.name }}</p>
+							<p>${{ item.price }}</p>
+							<div class="arrow-box">
+								<SvgIcon icon="angle-right" />
+							</div>
+						</text-content>
+
+						<edit-links v-if="ui.editProductMode">
+							<button class="tiny-voice">Quantity</button>
+							<button class="tiny-voice" @click="remove(`${item.id}`)">Delete</button>
+						</edit-links>
+					</cart-card>
+				</RouterLink>
 			</li>
 		</ul>
-		{{ cart.checkoutTotal }}
 
+		<button class="trash-can" v-if="cart.itemsInCart" @click="clear()">
+			<SvgIcon icon="trash" />
+		</button>
 		<RouterLink class="button strict-voice" @click="ui.specificToggle()" to="/checkout"
 			>Checkout</RouterLink
 		>
@@ -42,9 +77,9 @@
 		top: 0;
 		width: 100%;
 		height: 100%;
-		max-width: 75%;
+		max-width: 80%;
 		max-height: 85vh;
-		padding: 1rem 2rem;
+		padding: 1rem 1rem;
 
 		border-top-left-radius: 25px;
 		border-bottom-left-radius: 25px;
@@ -54,7 +89,7 @@
 		flex-direction: column;
 
 		overflow-y: auto;
-		opacity: 0.4;
+		opacity: 0;
 		transform: translate(100%, 0);
 		z-index: 200;
 		pointer-events: none;
@@ -71,10 +106,44 @@
 			border: none;
 			outline: none;
 			background-color: transparent;
+			cursor: pointer;
 		}
 
-		inner-column * {
-			opacity: 0;
+		.tiny-button {
+			position: absolute;
+			right: 10px;
+			top: 1rem;
+		}
+
+		.trash-can {
+			cursor: pointer;
+			position: absolute;
+			right: 10px;
+			bottom: 1.5rem;
+
+			.contains-svg {
+				width: 25px;
+				height: 30px;
+			}
+		}
+
+		ul {
+			flex: 1;
+			margin-top: 20px;
+			width: 100%;
+		}
+
+		cart-card text-content {
+			flex-direction: row;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			gap: 12px;
+			min-height: 12%;
+			font-weight: 600;
+			p:nth-of-type(2) {
+				flex: 1;
+			}
 		}
 	}
 	.cart-open cart-modal {
@@ -87,7 +156,16 @@
 		}
 	}
 
-	// header.toggle-open.cart-open cart-modal {
-	// 	top: 193px;
-	// }
+	edit-links {
+		margin-top: 5px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-around;
+		text-decoration: underline;
+		button,
+		a {
+			font-weight: 500;
+		}
+	}
 </style>
