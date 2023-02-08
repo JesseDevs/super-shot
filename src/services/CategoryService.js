dbf8c752d1cc8ef4fb827162d3c41b0a00e5780e
Jesse <jessemerge2@gmail.com>
2023-02-08 02:14:48 -0800

    Add category firebase storage service js file

---
+++ src/services/CategoryService.js
@@ -0,0 +1,68 @@
+import { reactive, ref } from "vue";
+import { useFirestore, useCollection } from "vuefire";
+import { collection, doc, addDoc, deleteDoc, setDoc, query, orderBy } from "firebase/firestore";
+import { defineStore } from "pinia";
+
+export const useCategoryService = defineStore("categories", function () {
+	const db = useFirestore();
+	const categoriesData = collection(db, "categories");
+	const sorted = query(categoriesData, orderBy("title"));
+	const sortedList = useCollection(sorted);
+	const editing = ref(false);
+
+	const form = reactive({
+		title: "",
+		info: "",
+		imageURL: "",
+	});
+
+	function clearForm() {
+		form.title = "";
+		form.info = "";
+		form.imageURL = "";
+	}
+
+	function addCategory() {
+		addDoc(collection(db, "categories"), {
+			title: form.title,
+			info: form.info,
+			imageURL: form.imageURL,
+		});
+		clearForm();
+	}
+
+	async function removeCategory(docID) {
+		const record = doc(db, "categories", docID);
+		if (confirm("Are you sure?")) {
+			await deleteDoc(record);
+		}
+	}
+
+	function editCategory(id) {
+		editing.value = id;
+	}
+
+	function updateCategory(id, c) {
+		setDoc(doc(db, "categories", id), {
+			title: c,
+		});
+		clearEdit();
+	}
+
+	function clearEdit() {
+		editing.value = false;
+	}
+
+	return {
+		db,
+		sortedList,
+		categoriesData,
+		editing,
+		form,
+		addCategory,
+		removeCategory,
+		editCategory,
+		updateCategory,
+		clearEdit,
+	};
+});
