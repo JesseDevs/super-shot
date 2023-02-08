64621499c466f821a5d29187c77d6b134bf54019
Jesse <jessemerge2@gmail.com>
2023-02-08 02:15:25 -0800

    Make a form to add a category to firebase

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,38 +1,83 @@
 <script setup>
-	import { computed } from "vue";
-	import MenuProductForm from "../components/MenuProductForm.vue";
-	import { useProfilesStore } from "../stores/Profiles";
-
-	const profiles = useProfilesStore();
-	const admin = computed(function () {
-		if (profiles.isLoggedIn && profiles.currentUser.role == "admin") {
-			return true;
-		} else return false;
-	});
+	import { useCategoryService } from "@/services/CategoryService";
+	const categoryService = useCategoryService();
 </script>
 
 <template>
-	<section>
-		<inner-column>
-			<create-thing v-if="admin">
-				<h2 class="chant-voice">Create A Product</h2>
-				<MenuProductForm />
-			</create-thing>
-			<create-thing v-else>
-				<h2 class="chant-voice">Woah. Looks like you don't have access to this page.</h2>
-				<p class="intro">Maybe sign in?</p>
-				<button class="button" @click="profiles.signIn(profile)">Sign In</button>
-			</create-thing>
-		</inner-column>
-	</section>
+	<create-thing>
+		<h2 class="chant-voice">Create A Category</h2>
+
+		<form class="main-form" autocomplete="off" @submit.prevent="categoryService.addCategory()">
+			<div class="form-field">
+				<label for="title">Title?</label>
+				<input id="title" type="text" required v-model="categoryService.form.title" />
+			</div>
+
+			<div class="form-field">
+				<label for="info">Info blurb?</label>
+				<input id="info" type="text" required v-model="categoryService.form.info" />
+			</div>
+
+			<div class="form-field">
+				<label for="image">Image URL?</label>
+				<input id="image" type="text" required v-model="categoryService.form.imageURL" />
+			</div>
+
+			<button class="button" type="submit">Add</button>
+		</form>
+
+		<ul>
+			<li v-for="category in categoryService.sortedList">
+				{{ category.title }}
+
+				<button @click="categoryService.removeCategory(category.id)" type="button">X</button>
+
+				<button @click="categoryService.editCategory(category.id)">Edit</button>
+
+				<div v-if="categoryService.editing === category.id" />
+				<input type="text" v-model="category.title" />
+				<button>Update</button>
+			</li>
+		</ul>
+	</create-thing>
 </template>
 <style lang="scss">
 	create-thing {
 		display: block;
 		max-width: 450px;
+	}
 
-		button {
-			margin-top: 20px;
+	form.main-form {
+		display: flex;
+		flex-direction: column;
+		gap: 20px;
+	}
+	div.form-field {
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		position: relative;
+
+		div {
+			width: 85%;
+		}
+
+		div.contains-textarea {
+			height: 100px;
+		}
+
+		select {
+			appearance: none;
+			border: 1px solid black;
+		}
+
+		label {
+			text-align: left;
+			font-weight: 500;
+			padding-left: 25px;
 		}
 	}
 </style>
