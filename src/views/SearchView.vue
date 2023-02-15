c54a6d162b3823db3444fa9e79ae2c286519ce30
Jesse <jessemerge2@gmail.com>
2023-02-14 17:02:14 -0800

    Fix search page from breaking - module thing

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -7,7 +7,7 @@
 	let searchString = ref('');
 
 	const filtered = computed(function () {
-		if (searchString.value.length > 1) {
+		if (searchString.value.length) {
 			return p.list.filter(function (item) {
 				var itemVariable = item.name.toUpperCase();
 				return itemVariable.includes(searchString.value.toUpperCase());
@@ -22,54 +22,31 @@
 <template>
 	<section>
 		<inner-column>
-			<search-div>
-				<ModuleThing :pageData="pageData" />
+			<search-div v-if="p">
+				<module-thing>
+					<h1 class="strict-voice">{{ pageData.title }}</h1>
+
+					<p class="intro">{{ pageData.subHeading }}</p>
+				</module-thing>
 
 				<form action="">
 					<div class="form-field">
-						<label
-							class="calm-voice"
-							for="search"
-							>Enter Product</label
-						>
-						<input
-							type="text"
-							id="search"
-							v-model="searchString"
-						/>
+						<label class="calm-voice" for="search">Enter Product</label>
+						<input type="text" id="search" v-model="searchString" />
 					</div>
 				</form>
-				<h5
-					class="strict-voice"
-					v-if="searchString !== ''"
-				>
-					Results for {{ searchString }}
-				</h5>
-				<h5
-					class="strict-voice"
-					v-else
-				>
-					Results for All Products
-				</h5>
+				<h5 class="strict-voice" v-if="searchString !== ''">Results for {{ searchString }}</h5>
+				<h5 class="strict-voice" v-else>Results for All Products</h5>
 			</search-div>
-			<ul
-				v-if="p.list"
-				class="main-grid"
-			>
+			<ul class="main-grid">
 				<li v-for="product in filtered">
 					<RouterLink :to="`menu/${product.category}/${product.id}`">
 						<general-card>
 							<picture class="item-picture">
-								<img
-									:src="`${product.imageURL}`"
-									alt="iced"
-									loading="lazy"
-								/>
+								<img :src="`${product.imageURL}`" alt="iced" loading="lazy" />
 							</picture>
 							<div class="name-highlight">
-								<span class="small-voice">{{
-									product.name.toUpperCase()
-								}}</span>
+								<span class="small-voice">{{ product.name.toUpperCase() }}</span>
 							</div>
 						</general-card>
 					</RouterLink>
