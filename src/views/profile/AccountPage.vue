edc6e5d79728224afc7232f5dbc2a72b4d886fd2
Jesse <jessemerge2@gmail.com>
2023-02-24 23:17:16 -0800

    WOrk on profile page

--- src/views/profile/AccountPage.vue
+++ src/views/profile/AccountPage.vue
@@ -1,10 +1,12 @@
 <script setup>
-	import { reactive, watch } from 'vue';
+	import { reactive, watch, ref } from 'vue';
 	import { useUserService } from '../../services/UserService';
 	const user = useUserService();
 
 	const form = reactive({
-		username: 'user.userDoc?.username',
+		username: user.username,
+		firstN: user.firstN,
+		lastN: user.lastN,
 	});
 
 	watch(user, function (a, b) {
@@ -13,51 +15,117 @@
 
 	const editingUsername = ref(false);
 	function toggleUsernameEdit() {
-		editingUsername.value != editingUsername.value;
+		editingUsername.value = !editingUsername.value;
 	}
+
+	function closeUsernameEdit() {
+		editingUsername.value = false;
+		window.scrollTo(0, 0);
+	}
+
 	function updateUsername() {
-		if (user.changeUsername(form.username)) {
+		if (user.changeUsername(form.username, form.firstN, form.lastN)) {
 			editingUsername.value = false;
 		}
 	}
 </script>
 
 <template>
-	<h1>Account Page</h1>
-	<p v-if="user.authUser" class="username">{{ user.authUser.email }}</p>
+	{{ user.firstN }}
+	<account-page v-if="user">
+		<div class="account-display-box">
+			<picture>
+				<img src="@/assets/profile/profile.webp" alt="pic" />
+			</picture>
+		</div>
+		<div class="account-display-box">
+			<p>{{ user.authUser?.email }}</p>
+			<h6 class="small-voice">Email</h6>
+		</div>
+		<div class="account-display-box">
+			<p>
+				{{ user?.username }}
+			</p>
+			<h6 class="small-voice">Username</h6>
+		</div>
+		<div class="account-display-box">
+			<p>
+				{{ user?.firstN }}
+			</p>
+			<h6 class="small-voice">First Name</h6>
+		</div>
+		<div class="account-display-box">
+			<p>
+				{{ user?.lastN }}
+			</p>
+			<h6 class="small-voice">Last Name</h6>
+		</div>
 
-	<p v-if="user.authUser" class="username">
-		{{ user?.username }}
-	</p>
-	<button type="button" @click="toggleUsernameEdit">Edit</button>
+		<button v-if="!editingUsername" class="button" type="button" @click="toggleUsernameEdit()">
+			Personalize
+		</button>
 
-	<Transition>
-		<form
-			@sumbit.prevent="user.changeUsername(form.username)"
-			class="update-username"
-			v-if="editingUsername"
-		>
-			<div class="form-field">
-				<label for="username">Username <span>*</span> </label>
-				<input id="username" type="text" required v-model="form.lastName" />
-			</div>
-			<button type="submit">Update</button>
-			<button type="submit">Cancel</button>
-			<!-- close toggle to change username -->
-		</form>
-	</Transition>
+		<Transition>
+			<form
+				@sumbit.prevent="user.changeUsername(form.username)"
+				class="update-username main-form"
+				v-if="editingUsername"
+			>
+				<div class="form-field">
+					<label for="username">Username </label>
+					<input id="username" type="text" required v-model="form.username" />
+				</div>
+
+				<div class="form-field">
+					<label for="firstN">First Name</label>
+					<input id="firstN" type="text" required v-model="form.firstN" />
+				</div>
+
+				<div class="form-field">
+					<label for="lastN">Last Name</label>
+					<input id="lastN" type="text" required v-model="form.lastN" />
+				</div>
+
+				<actions-block>
+					<button class="button" type="submit">Update</button>
+					<button class="button" type="button" @click.prevent="closeUsernameEdit()">Cancel</button>
+				</actions-block>
+			</form>
+		</Transition>
+	</account-page>
 </template>
 
 <style lang="scss">
-	p.username {
-		background-color: red;
-		width: 200px;
-		height: 50px;
+	account-page {
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		> button {
+			align-self: flex-end;
+			margin-top: 20px;
+		}
 	}
+	.account-display-box {
+		display: flex;
+		flex-direction: column;
+		position: relative;
 
+		p {
+			border-bottom: 1px solid var(--color);
+			min-height: 24px;
+		}
+		h6 {
+			align-self: flex-end;
+			pointer-events: none;
+		}
+
+		picture {
+			margin: 0;
+		}
+	}
 	.v-enter-active,
 	.v-leave-active {
-		transition: opacity 0.5s ease;
+		transition: opacity 0.3s ease-in-out;
 	}
 
 	.v-enter-from,
--- src/views/profile/ProfileView.vue
+++ src/views/profile/ProfileView.vue
@@ -1,23 +1,58 @@
-<script setup></script>
+<script setup>
+	import { useUserService } from '@/services/UserService';
+
+	const user = useUserService();
+</script>
 
 <template>
-	<h1>profile</h1>
+	<project-page>
+		<h1 class="loud-voice">Welcome, {{ user.username }}</h1>
+		<nav>
+			<ul>
+				<li>
+					<RouterLink to="/profile"> Account </RouterLink>
+				</li>
+				<li>
+					<RouterLink to="/profile/billing"> Billing </RouterLink>
+				</li>
+				<li>
+					<RouterLink to="/profile/favorites"> Favorites </RouterLink>
+				</li>
+			</ul>
+		</nav>
 
-	<ul>
-		<li>
-			<RouterLink to="/profile"> Account </RouterLink>
-		</li>
-		<li>
-			<RouterLink to="/profile/billing"> Billing </RouterLink>
-		</li>
-		<li>
-			<RouterLink to="/profile/favorites"> Favorites </RouterLink>
-		</li>
-	</ul>
-
-	<main class="outlet">
-		<RouterView />
-	</main>
+		<main class="outlet">
+			<RouterView />
+		</main>
+	</project-page>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+	project-page {
+		display: flex;
+		flex-direction: column;
+		gap: 20px;
+
+		nav ul {
+			display: flex;
+			justify-content: space-evenly;
+			gap: 1px;
+
+			li {
+				width: 100%;
+			}
+			a {
+				padding: 10px;
+				border: 2px solid var(--color);
+				background-color: var(--color-mute);
+				color: var(--page);
+				transition: all 0.2s ease-in-out;
+
+				&:hover {
+					background-color: var(--color-soft);
+					color: var(--black);
+				}
+			}
+		}
+	}
+</style>
