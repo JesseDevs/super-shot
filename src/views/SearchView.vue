7cfaf6ee59f9e5c54f96b014c35015cf77c7c3ca
Jesse <jessemerge2@gmail.com>
2023-03-18 21:03:35 -0700

    redesign search page

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -33,8 +33,8 @@
 				<input type="text" id="search" v-model="searchString" />
 			</div>
 		</form>
-		<h5 class="strict-voice" v-if="searchString !== ''">Results for {{ searchString }}</h5>
-		<h5 class="strict-voice" v-else>Results for All Products</h5>
+		<h5 class="strict-voice" v-if="searchString !== ''">Results: {{ searchString }}</h5>
+		<h5 class="strict-voice" v-else>Results: All&nbsp;Products</h5>
 	</search-div>
 	<ul class="main-grid">
 		<li v-for="product in filtered">
@@ -54,8 +54,21 @@
 
 <style lang="scss">
 	search-div {
+		h5 {
+			text-transform: uppercase;
+		}
 		form {
-			max-width: 400px;
+			max-width: 500px;
+			width: 100%;
+
+			label {
+				max-width: fit-content;
+				padding-bottom: 10px;
+			}
+
+			input {
+				flex-grow: 1;
+			}
 		}
 		label {
 			font-weight: 500;
@@ -65,5 +78,16 @@
 		flex-direction: column;
 		gap: 15px;
 		padding-bottom: 15px;
+		align-items: center;
+
+		@media (min-width: 450px) {
+			.form-field {
+				flex-direction: row;
+				label {
+					padding: 0;
+					padding-right: 20px;
+				}
+			}
+		}
 	}
 </style>
