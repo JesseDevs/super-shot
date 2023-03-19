a57b8a94fc93dcecea1df371575d31dda5c83095
Jesse <jessemerge2@gmail.com>
2023-03-18 20:35:49 -0700

    Show code compoenent

---
+++ src/components/ShowCode.vue
@@ -0,0 +1,14 @@
+<script setup>
+	defineProps(['code']);
+</script>
+<template>
+	<pre><code>
+		{{ code }}
+	</code></pre>
+</template>
+
+<style scoped lang="scss">
+	pre {
+		border: 1px solid red;
+	}
+</style>
